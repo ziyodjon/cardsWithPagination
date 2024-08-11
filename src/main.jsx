a4ffeaa104e9b4867f74f9f15dc5ce407138248d
Sysadmin <44997750+ziyodjon@users.noreply.github.com>
2024-08-11 22:03:09 +0500

    lastChanges

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,3 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
-//   const [datas, setDatas] = useState([]);
-
-//   useEffect(() => {
-//     fetch(
-//       `https://dummyjson.com/products?limit=6&skip=1&select=title,price,images,description`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => setDatas(data))
-//       .catch((error) => console.error("Error:", error));
-//   }, []);
-
-//   return (
-//     <div className="products">
-//       <ul className="items">
-//         <li>{"Items 12qeq"}</li>
-//         {console.log(datas)}
-//       </ul>
-//     </div>
-//   );
